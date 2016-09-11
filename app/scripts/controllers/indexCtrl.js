73530e98e2644dbacbdea8a349c9baa52cef50f5
Daniel Cudney <upnvapesd@gmail.com>
2016-09-11 10:02:19 -0700

    JS

--- app/scripts/controllers/indexCtrl.js
+++ app/scripts/controllers/indexCtrl.js
@@ -3,8 +3,7 @@ angular.module("incly")
 .controller("indexCtrl", function($scope, $interval, encryptionService, dataService) {
 
   dataService.getItems(function(databaseLoadForIndex) {
-    console.log(databaseLoadForIndex);
-    $scope.databaseLoad = databaseLoadForIndex;
+    $scope.databaseLoad = databaseLoadForIndex.data;
   })
 
 
