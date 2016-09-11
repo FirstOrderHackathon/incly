a6f2fc5632647983b33799084a6fbb479ef1d918
Daniel Cudney <upnvapesd@gmail.com>
2016-09-10 20:09:22 -0700

    Fixed

--- app/scripts/config/main.js
+++ app/scripts/config/main.js
@@ -3,26 +3,16 @@ var incly = angular.module("incly", [require('angular-ui-router')])
 incly.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
   $urlRouterProvider.otherwise('/');
   $stateProvider
-    .state('index', {
-      url: '/',
-      templateUrl: 'index.html',
-      controller: 'indexCtrl'
-    })
     .state('home', {
-      url: '/view',
+      url: '/',
       templateUrl: 'views/view/view.html',
       controller: 'indexCtrl'
     })
     .state('add_edit', {
       url: '/smile',
       templateUrl: 'views/add_edit/add_edit.html',
-      controller: 'indexCtrl'
-    })
-
-    .state('smile', {
-      url: '/smile',
-      templateUrl: 'views/main/index.html',
       controller: 'smileCtrl'
     })
+
 }])
 incly.run(['$state', function($state){}]);
