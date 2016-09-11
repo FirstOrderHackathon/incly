8c97f4afe1efaaacd22fac64319b05faba062891
Daniel Cudney <upnvapesd@gmail.com>
2016-09-10 18:04:29 -0700

    Data Service W/O URL(s)

--- public/scripts/incly.bundle.js
+++ public/scripts/incly.bundle.js
@@ -11,6 +11,7 @@ webpackJsonp([0],[
 	__webpack_require__(5);
 	//SERVICE
 	__webpack_require__(6);
+	__webpack_require__(7);
 
 
 /***/ },
@@ -4686,6 +4687,49 @@ webpackJsonp([0],[
 
 
 
+
+
+
+	});
+
+
+/***/ },
+/* 7 */
+/***/ function(module, exports) {
+
+	'use strict';
+	angular.module("incly")
+	.service("dataService", function($http, $q) {
+
+	  var getUrl = ""; // Get a List of Items in the Database
+	  var putUrl = ""; // Save a Individual List
+	  var deleteUrl = ""; // Delete a Single Item
+	  var postUrl = ""; // Post New Item into the Database
+	  var getUrlSingleItem = ""; // Single Item Get
+
+	//Full Database Get
+	  this.getItems = function(callback) {
+	    $http.get(getUrl)
+	      .then(callback)
+	  };
+	  //Single Items CRUD Requests
+	  this.saveItem = function(id, product, callback) {
+	    $http.put(putUrl + id, product)
+	      .then(callback)
+	  };
+	  this.deleteItem = function(id, callback) {
+	    var tempUrl = deleteUrl + id;
+	    $http.delete(tempUrl)
+	    .then(callback);
+	  };
+	  this.newItem = function(post, callback) {
+	    $http.post(postUrl, post)
+	    .then(callback);
+	  };
+	  this.getItem = function(callback) {
+	    $http.get(getUrlSingleItem)
+	      .then(callback)
+	  };
 
 
 
