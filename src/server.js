4fb5955ef44fcbc88ddff5f81595c65c7a024dbb
Orlando Elias <oelias27@gmail.com>
2016-09-10 23:08:56 -0700

    Added API endpoint for uploading files

--- src/server.js
+++ src/server.js
@@ -1,23 +1,32 @@
 var express = require('express');
+var busboy = require('connect-busboy');
 var bodyParser = require('body-parser')
 var morgan = require('morgan');
 var mongoose = require('mongoose');
 var routes = require('./routes/routes.js');
 var dbInfo = require('./dbInfo.js');
+var passport = require('passport');
+var passportStrategy = require('./passport/passport.js');
+var path = require('path');
 
 var port = process.env.PORT || 8080;
 
-console.log(dbInfo.url)
-mongoose.connect(dbInfo.url);
+// console.log(dbInfo.url)
+// mongoose.connect(dbInfo.url);
+
+mongoose.connect('mongodb://localhost:27017/incly');
 var connection = mongoose.connection;
 
 var app = express();
 
-app.use(express.static('public'))
+app.use(express.static('../public'))
 .use(bodyParser())
 .use(morgan('dev'))
+.use(passport.initialize())
+.use(busboy());
 
-routes(app, connection)
+passportStrategy(passport);
+routes(app, connection, passport);
 
 app.listen(port, function() {
   console.log('Server running on port', port)
